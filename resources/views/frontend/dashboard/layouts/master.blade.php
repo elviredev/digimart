c9d93c8fa2d9f511bfae0352e52e1a7acaebb84e
elviredev <sandrine.rodriguez@hotmail.com>
2025-12-21 18:34:32 +0100

    🐈 Logout feature & Register Page.

--- resources/views/frontend/dashboard/layouts/master.blade.php
+++ resources/views/frontend/dashboard/layouts/master.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Title -->
-  <title> Digital Market Place HTML Template</title>
+  <title> Dashboard </title>
   <!-- Favicon -->
   <link rel="shortcut icon" href="{{ asset('assets/frontend/images/logo/favicon-two.png') }}">
 
@@ -21,9 +21,10 @@
   <!-- line awesome -->
   <link rel="stylesheet" href="{{ asset('assets/frontend/css/line-awesome.min.css') }}">
   <!-- Tabler Icons CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tabler-icons/3.21.0/tabler-icons.min.css"
+  <link
+  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tabler-icons/3.21.0/tabler-icons.min.css"
   integrity="sha512-XrgoTBs7P5YtpkeKqKOKkruURsawIaRrhe8QrcWeMnFeyRZiOcRNjBAX+AQeXOvx9/9fSY32dVct1PccRoCICQ=="
-  crossorigin="anonymous" referrerpolicy="no-referrer" />
+  crossorigin="anonymous" referrerpolicy="no-referrer"/>
   <!-- Main css -->
   <link rel="stylesheet" href="{{ asset('assets/frontend/css/main.css') }}">
 
@@ -51,14 +52,16 @@
 <!-- ==================== Scroll to Top End Here ==================== -->
 <div class="progress-wrap">
   <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-    <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"/>
   </svg>
 </div>
 <!-- ==================== Scroll to Top End Here ==================== -->
 
 <!-- ==================== Mobile Menu Start Here ==================== -->
 <div class="mobile-menu d-lg-none d-block">
-  <button type="button" class="close-button"> <i class="las la-times"></i> </button>
+  <button type="button" class="close-button">
+    <i class="las la-times"></i>
+  </button>
   <div class="mobile-menu__inner">
     <a href="index.html" class="mobile-menu__logo">
       <img src="assets/images/logo/logo-two.png" alt="Logo" class="white-version">
@@ -71,14 +74,22 @@
         </a>
 
         <div class="dropdown">
-          <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
+          <button
+          class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
           aria-expanded="false">
             <img src="assets/images/icons/user.svg" alt="">
           </button>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Sign Up</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li>
+              <a class="dropdown-item" href="#">Sign
+                Up</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Profile</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Settings</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -88,13 +99,19 @@
           <a href="javascript:void(0)" class="nav-menu__link">Home</a>
           <ul class="nav-submenu">
             <li class="nav-submenu__item">
-              <a href="index.html" class="nav-submenu__link"> Home One</a>
+              <a href="index.html" class="nav-submenu__link">
+                Home
+                One</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="index-two.html" class="nav-submenu__link"> Home Two</a>
+              <a href="index-two.html" class="nav-submenu__link">
+                Home
+                Two</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="index-three.html" class="nav-submenu__link"> Home Three</a>
+              <a href="index-three.html" class="nav-submenu__link">
+                Home
+                Three</a>
             </li>
           </ul>
         </li>
@@ -102,10 +119,14 @@
           <a href="javascript:void(0)" class="nav-menu__link">Products</a>
           <ul class="nav-submenu">
             <li class="nav-submenu__item">
-              <a href="all-product.html" class="nav-submenu__link"> All Products</a>
+              <a href="all-product.html" class="nav-submenu__link">
+                All
+                Products</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="product-details.html" class="nav-submenu__link"> Product Details</a>
+              <a href="product-details.html" class="nav-submenu__link">
+                Product
+                Details</a>
             </li>
           </ul>
         </li>
@@ -113,22 +134,32 @@
           <a href="javascript:void(0)" class="nav-menu__link">Pages</a>
           <ul class="nav-submenu">
             <li class="nav-submenu__item">
-              <a href="profile.html" class="nav-submenu__link"> Profile</a>
+              <a href="profile.html" class="nav-submenu__link">
+                Profile</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="cart.html" class="nav-submenu__link"> Shopping Cart</a>
+              <a href="cart.html" class="nav-submenu__link">
+                Shopping
+                Cart</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="cart-personal.html" class="nav-submenu__link"> Mailing Address</a>
+              <a href="cart-personal.html" class="nav-submenu__link">
+                Mailing
+                Address</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="cart-payment.html" class="nav-submenu__link"> Payment Method</a>
+              <a href="cart-payment.html" class="nav-submenu__link">
+                Payment
+                Method</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="cart-thank-you.html" class="nav-submenu__link"> Preview Order</a>
+              <a href="cart-thank-you.html" class="nav-submenu__link">
+                Preview
+                Order</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="dashboard.html" class="nav-submenu__link"> Dashboard</a>
+              <a href="dashboard.html" class="nav-submenu__link">
+                Dashboard</a>
             </li>
           </ul>
         </li>
@@ -136,13 +167,19 @@
           <a href="javascript:void(0)" class="nav-menu__link">Blog</a>
           <ul class="nav-submenu">
             <li class="nav-submenu__item">
-              <a href="blog.html" class="nav-submenu__link"> Blog</a>
+              <a href="blog.html" class="nav-submenu__link">
+                Blog</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="blog-details.html" class="nav-submenu__link"> Blog Details</a>
+              <a href="blog-details.html" class="nav-submenu__link">
+                Blog
+                Details</a>
             </li>
             <li class="nav-submenu__item">
-              <a href="blog-details-sidebar.html" class="nav-submenu__link"> Blog Details Sidebar</a>
+              <a href="blog-details-sidebar.html" class="nav-submenu__link">
+                Blog
+                Details
+                Sidebar</a>
             </li>
           </ul>
         </li>
@@ -160,72 +197,7 @@
   <div class="dashboard__inner d-flex">
 
     <!-- ===================== Dashboard Sidebar Start ======================= -->
-    <div class="dashboard-sidebar">
-      <button type="button" class="dashboard-sidebar__close d-lg-none d-flex"><i
-        class="las la-times"></i></button>
-      <div class="dashboard-sidebar__inner">
-        <a href="index.html" class="logo mb-48">
-          <img src="assets/images/logo/logo.png" alt="" class="white-version">
-        </a>
-        <a href="index.html" class="logo logo_icon favicon mb-48">
-          <img src="assets/images/thumbs/dashboard_sidebar_icon.png" alt="">
-        </a>
-
-        <!-- Sidebar List Start -->
-        <ul class="sidebar-list">
-          <li class="sidebar-list__item">
-            <a href="dashboard.html" class="sidebar-list__link">
-              <span class="sidebar-list__icon">
-                  <i class="ti ti-device-heart-monitor"></i>
-              </span>
-              <span class="text">Dashboard</span>
-            </a>
-          </li>
-          <li class="sidebar-list__item">
-            <a href="dashboard-profile.html" class="sidebar-list__link">
-              <span class="sidebar-list__icon">
-                  <i class="ti ti-user"></i>
-              </span>
-              <span class="text">Profile</span>
-            </a>
-          </li>
-          <li class="sidebar-list__item">
-            <a href="setting.html" class="sidebar-list__link">
-              <span class="sidebar-list__icon">
-                  <i class="ti ti-settings"></i>
-              </span>
-              <span class="text">Settings</span>
-            </a>
-          </li>
-          <li class="sidebar-list__item">
-            <a href="dashboard-table.html" class="sidebar-list__link">
-              <span class="sidebar-list__icon">
-                  <i class="ti ti-list-details"></i>
-              </span>
-              <span class="text">Table Design</span>
-            </a>
-          </li>
-          <li class="sidebar-list__item">
-            <a href="dashboard-form.html" class="sidebar-list__link">
-              <span class="sidebar-list__icon">
-                  <i class="ti ti-list"></i>
-              </span>
-              <span class="text">Form Design</span>
-            </a>
-          </li>
-          <li class="sidebar-list__item">
-            <a href="login.html" class="sidebar-list__link">
-              <span class="sidebar-list__icon">
-                  <i class="ti ti-logout"></i>
-              </span>
-              <span class="text">Logout</span>
-            </a>
-          </li>
-        </ul>
-        <!-- Sidebar List End -->
-
-      </div>
-    </div>
+    @include('frontend.dashboard.layouts.sidebar')
     <!-- ===================== Dashboard Sidebar End ======================= -->
 
     <div class="dashboard-body">
@@ -244,7 +216,8 @@
                                 <img src="assets/images/icons/search-dark.svg" alt="" class="white-version">
                                 <img src="assets/images/icons/search-dark-white.svg" alt="" class="dark-version">
                             </span>
-            <input type="text" class="common-input common-input--md common-input--bg pill w-100"
+            <input
+            type="text" class="common-input common-input--md common-input--bg pill w-100"
             placeholder="Search here...">
           </form>
         </div>
@@ -262,9 +235,11 @@
                   <li class="sidebar-list__item">
                     <a href="dashboard-profile.html" class="sidebar-list__link">
                                                 <span class="sidebar-list__icon">
-                                                    <img src="assets/images/icons/sidebar-icon2.svg" alt=""
+                                                    <img
+                                                    src="assets/images/icons/sidebar-icon2.svg" alt=""
                                                     class="icon">
-                                                    <img src="assets/images/icons/sidebar-icon-active2.svg" alt=""
+                                                    <img
+                                                    src="assets/images/icons/sidebar-icon-active2.svg" alt=""
                                                     class="icon icon-active">
                                                 </span>
                       <span class="text">Profile</span>
@@ -274,9 +249,11 @@
                   <li class="sidebar-list__item">
                     <a href="setting.html" class="sidebar-list__link">
                                                 <span class="sidebar-list__icon">
-                                                    <img src="assets/images/icons/sidebar-icon10.svg" alt=""
+                                                    <img
+                                                    src="assets/images/icons/sidebar-icon10.svg" alt=""
                                                     class="icon">
-                                                    <img src="assets/images/icons/sidebar-icon-active10.svg" alt=""
+                                                    <img
+                                                    src="assets/images/icons/sidebar-icon-active10.svg" alt=""
                                                     class="icon icon-active">
                                                 </span>
                       <span class="text">Settings</span>
@@ -285,9 +262,11 @@
                   <li class="sidebar-list__item">
                     <a href="login.html" class="sidebar-list__link">
                                                 <span class="sidebar-list__icon">
-                                                    <img src="assets/images/icons/sidebar-icon13.svg" alt=""
+                                                    <img
+                                                    src="assets/images/icons/sidebar-icon13.svg" alt=""
                                                     class="icon">
-                                                    <img src="assets/images/icons/sidebar-icon-active13.svg" alt=""
+                                                    <img
+                                                    src="assets/images/icons/sidebar-icon-active13.svg" alt=""
                                                     class="icon icon-active">
                                                 </span>
                       <span class="text">Logout</span>
@@ -298,13 +277,22 @@
 
               <div class="language-select flx-align select-has-icon">
                 <img src="assets/images/icons/globe.svg" alt="" class="globe-icon white-version">
-                <img src="assets/images/icons/globe-white.svg" alt=""
+                <img
+                src="assets/images/icons/globe-white.svg" alt=""
                 class="globe-icon dark-version">
                 <select class="select py-0 ps-2 border-0 fw-500">
-                  <option value="1">Eng</option>
-                  <option value="2">Bn</option>
-                  <option value="3">Eur</option>
-                  <option value="4">Urd</option>
+                  <option value="1">
+                    Eng
+                  </option>
+                  <option value="2">
+                    Bn
+                  </option>
+                  <option value="3">
+                    Eur
+                  </option>
+                  <option value="4">
+                    Urd
+                  </option>
                 </select>
               </div>
             </div>
@@ -321,13 +309,8 @@
 
       <!-- ====================== Dashboard Footer Start ======================== -->
       <div class="dashboard-footer bottom-footer-two mt-32 border-0 bg-white">
-        <div class="bottom-footer__inner flx-between gap-3">
-          <p class="bottom-footer__text font-14"> Copyright © 2024 DigiMart, All rights reserved.</p>
-          <div class="footer-links gap-4">
-            <a href="#" class="footer-link hover-text-heading font-14">Terms of service</a>
-            <a href="#" class="footer-link hover-text-heading font-14">Privacy Policy</a>
-            <a href="#" class="footer-link hover-text-heading font-14">cookies</a>
-          </div>
+        <div class="bottom-footer__inner text-end">
+          <p class="bottom-footer__text font-14">Copyright ©{{ Date('Y') }} DigiMart, All rights reserved.</p>
         </div>
       </div>
       <!-- ====================== Dashboard Footer End ======================== -->
